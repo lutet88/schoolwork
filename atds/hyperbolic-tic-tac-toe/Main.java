0d522a5b74cb8476b5f70a5c9220ab8f963bd33d
lutet / jeff <xhex88jeff@gmail.com>
2021-09-28 15:02:58 +0800

    in class 9/28

--- atds/hyperbolic-tic-tac-toe/Main.java
+++ atds/hyperbolic-tic-tac-toe/Main.java
@@ -5,7 +5,7 @@ class Main {
 
     public static void main(String[] args){
         System.out.println("bruh gaming");
-        Pentagon origin = Pentagon.createPentaGrid(2);
-        System.out.println(origin.getNeighbor(6));
+        Pentagon origin = Pentagon.createPentaGrid(3);
+        System.out.println(origin.toString());
     }
 }
--- atds/hyperbolic-tic-tac-toe/Pentagon.java
+++ atds/hyperbolic-tic-tac-toe/Pentagon.java
@@ -1,31 +1,50 @@
-
+import java.util.*;
 
 class Pentagon {
 
-    private int layer;
+    private final int PRINT_MAX_DEPTH = 3;
+
+    public int layer;
 
     public static Pentagon createPentaGrid(int maxDepth){
-        return new Pentagon(maxDepth);
+        return new Pentagon(maxDepth-1, 0);
     }
 
     private Pentagon[] neighbors = new Pentagon[10];
 
-    public Pentagon(Pentagon parent, int parentSide, int maxDepth) {
+    public Pentagon(Pentagon parent, int parentSide, int layer) {
         neighbors[parentSide] = parent;
+        this.layer = layer;
+    }
 
-        layer = maxDepth;
-        if (maxDepth == 0) return;
+    public void populate(int maxLayers){
+        System.out.println(layer);
+        if (layer >= maxLayers) return;
         for (int i = 0; i < 10; i++){
-            if (i == parentSide) continue;
-            neighbors[i] = new Pentagon(this, (i+5) % 10, maxDepth-1);
+            if (neighbors[i] == null) neighbors[i] = new Pentagon(this, (i+5) % 10, layer+1);
+        }
+        for (int i = 0; i < 10; i++){
+            if (neighbors[i].layer <= layer) continue;
+            setStartingNeighbors(i, neighbors);
+            neighbors[i].populate(maxLayers);
         }
     }
-    public Pentagon(int maxDepth){
-        layer = maxDepth;
-        if (maxDepth == 0) return;
-        for (int i = 0; i < 10; i++){
-            neighbors[i] = new Pentagon(this, (i+5) % 10, maxDepth-1);
-            neighbors[i].setNeighbor((i+4) % 10, neighbors[(i+9) % 10]);
+    public Pentagon(int maxLayers, int layer){
+        this.populate(maxLayers);
+        layer = layer;
+    }
+
+    private void setStartingNeighbors(int i, Pentagon[] nbs){
+        if (i % 2 == layer % 2) {
+            // side
+            nbs[i].setNeighbor((i+4) % 10, nbs[(i+2) % 10]);
+            nbs[i].setNeighbor((i+6) % 10, nbs[(i+8) % 10]);
+            nbs[i].setNeighbor((i+3) % 10, nbs[(i+1) % 10]);
+            nbs[i].setNeighbor((i+7) % 10, nbs[(i+9) % 10]);
+        } else {
+            // corner
+            nbs[i].setNeighbor((i+4) % 10, nbs[(i+1) % 10]);
+            nbs[i].setNeighbor((i+6) % 10, nbs[(i+9) % 10]);
         }
     }
 
@@ -43,18 +62,26 @@ class Pentagon {
         sb.append("Pentagon on layer "+layer+"\n");
         for(int i = 0; i < 10; i++){
             if (neighbors[i] == null) continue;
-            sb.append("\t"+i+": "+neighbors[i].toString(i, 2)+"\n");
+            sb.append("\t"+i+": "+neighbors[i].toString(i, PRINT_MAX_DEPTH)+"\n");
         }
         return sb.toString();
     }
 
     public String toString(int side, int tabs){
+        if (tabs > PRINT_MAX_DEPTH) return this.hashCode()+" ...";
         StringBuilder sb = new StringBuilder();
-        sb.append("Pentagon on layer "+layer);
-        for (int i = side+8; i <= side+11; i++){
+        sb.append("Pentagon "+this.hashCode()+" on layer "+layer);
+        for (int i = 0; i < 10; i++){
             if (neighbors[i % 10] == null) continue;
+
             sb.append("\n");
             for (int j = 0; j < tabs; j++) sb.append("\t");
+
+            if ((i + 5) % 10 == side) {
+                sb.append(i%10 + ": Parent");
+                continue;
+            }
+
             sb.append(i%10+": "+neighbors[i % 10].toString(i % 10, tabs+1));
         }
         return sb.toString();
